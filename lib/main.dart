269c5870607926c2bd46197803f22201c5c6a014
greendrop <greendrop.devel@gmail.com>
2020-08-23 15:47:31 +0900

    add analysis_options.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -97,7 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
+            const Text(
               'You have pushed the button this many times:',
             ),
             Text(
