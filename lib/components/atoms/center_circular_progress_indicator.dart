f8b3319928f2581286405f1e0c680f4b667ec41d
greendrop <greendrop.devel@gmail.com>
2021-12-19 05:43:27 +0900

    init commit

--- lib/components/atoms/center_circular_progress_indicator.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class CenterCircularProgressIndicator extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return const Center(child: CircularProgressIndicator(), heightFactor: 2);
-  }
-}
